8a26e2e9c585b93504fc06c54d872f06c2c5e8fd
minh12200412~ <minhvuive122004@gmail.com>
2024-07-26 00:07:42 +0700

    CompleteEate

--- app.js
+++ app.js
@@ -105,3 +105,12 @@ navLinks.forEach((link) => {
     }
   });
 });
+
+function toggleMenu() {
+  var navMenu = document.getElementById("nav-menu");
+  if (navMenu.classList.contains("active")) {
+    navMenu.classList.remove("active");
+  } else {
+    navMenu.classList.add("active");
+  }
+}
